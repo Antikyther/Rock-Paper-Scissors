26b304cb2102915a8a80ea275ad4f99138786305
Olivia Shaw <loulshaw42@gmail.com>
2022-03-31 19:08:53 -0400

    Fixed function playRound

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,51 +1,40 @@
 // part 1 - computer selection
 
 function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-  }
-  
-function computerPlay(){
+  return Math.floor(Math.random() * max);
+}
+
+function computerPlay() {
   //Block Scope //
-  if (getRandomInt(2) == 0){
-    return 'Scissors';}
-    
- else if (getRandomInt(2) == 1){
-    return 'Rock';}
-        
- else{
-    return 'Paper'; }
-}
-
-// part 2 - player vs computer 
-
-function playRound(playerSelection, computerSelection) {
-  
-  if (playerSelection == 'rock' && computerSelection == 'paper') {
-    return `You Lose! ${playerSelection} beats ${computerSelection}`;
-
+  if (getRandomInt(2) == 0) {
+    return "scissors";
+  } else if (getRandomInt(2) == 1) {
+    return "rock";
+  } else {
+    return "paper";
   }
-
 }
 
- // return `You Lose! ${playerSelection} beats ${computerSelection}`;
+const playerSelection = "rock";
+const computerSelection = computerPlay();
 
-// Pseudo Code 
+// part 2 - player vs computer
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+  if (playerSelection == "rock" && computerSelection == "scissors") {
+    return `You Lose! ${playerSelection} beats ${computerSelection}`;
+  } else if (playerSelection == "paper" && computerSelection == "rock") {
+    return `You Lose! ${playerSelection} beats ${computerSelection}`;
+  } else if (playerSelection == "scissors" && computerSelection == "paper") {
+    return `You Lose! ${playerSelection} beats ${computerSelection}`;
+  } else {
+    return "You Win!";
+  }
+}
 
-  
-  
-  
-
-// if player choose rock and computer chooses paper 
-  // return player selection and computer selection and You lose 
-  // if player chooses scissors and computer chooses rock 
-  // return You lose! and the selection 
-  
-  // else return You Win!
-
- 
-// In JavaScript, scope refers to the visibility of variables. 
-// Variables which are defined outside of a function block have Global scope.
-let playerSelection = "rock";
-let computerSelection = computerPlay();
 console.log(playRound(playerSelection, computerSelection));
-
+// return `You Lose! ${playerSelection} beats ${computerSelection}`;
+// player and computer throw a hand
+// Rock beats scissors
+// paper beats rock
+// scissors beats paper
