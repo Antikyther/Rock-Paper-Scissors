14dd11b9b6877d7dc090c17eee5ceb8c678eb414
Olivia Shaw <loulshaw42@gmail.com>
2022-04-03 22:41:00 -0400

    Place variables inside scope of game function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -7,34 +7,46 @@ function getRandomInt(max) {
 function computerPlay() {
   //Block Scope //
   if (getRandomInt(2) == 0) {
-    return "scissors";
+    return "Scissors";
   } else if (getRandomInt(2) == 1) {
-    return "rock";
+    return "Rock";
   } else {
-    return "paper";
+    return "Paper";
   }
 }
-
-const playerSelection = "rock";
-const computerSelection = computerPlay();
 
 // part 2 - player vs computer
 function playRound(playerSelection, computerSelection) {
-  playerSelection = playerSelection.toLowerCase();
-  if (playerSelection == "rock" && computerSelection == "scissors") {
-    return `You Lose! ${playerSelection} beats ${computerSelection}`;
-  } else if (playerSelection == "paper" && computerSelection == "rock") {
-    return `You Lose! ${playerSelection} beats ${computerSelection}`;
-  } else if (playerSelection == "scissors" && computerSelection == "paper") {
-    return `You Lose! ${playerSelection} beats ${computerSelection}`;
+  let player = playerSelection.toLowerCase();
+  let computer = computerSelection.toLowerCase();
+  if (computer == player) {
+    return "Tie! Play again!";
+  } else if (computer == "rock" && player == "scissors") {
+    return `You Lose! ${computerSelection} beats ${playerSelection}`;
+  } else if (computer == "paper" && player == "rock") {
+    return `You Lose! ${computerSelection} beats ${playerSelection}`;
+  } else if (computer == "scissors" && player == "paper") {
+    return `You Lose! ${computerSelection} beats ${playerSelection}`;
   } else {
-    return "You Win!";
+    return `You Win! ${playerSelection} beats ${computerSelection}`;
   }
 }
 
-console.log(playRound(playerSelection, computerSelection));
-// return `You Lose! ${playerSelection} beats ${computerSelection}`;
-// player and computer throw a hand
-// Rock beats scissors
-// paper beats rock
-// scissors beats paper
+function game() {
+  for (let i = 0; i < 5; i++) {
+    let winner = playRound(playerSelection, computerSelection);
+    console.log(winner);
+  }
+}
+
+//Write a NEW function called game().
+//Call the playRound function inside of
+//this one to play a 5 round game that keeps
+//score and reports a winner or loser at the end.
+
+// Global Variables
+sign = window.prompt("Try your hand at rock-paper-scissors!");
+let playerSelection = sign;
+let computerSelection = computerPlay();
+//console.log(playRound(playerSelection, computerSelection));
+console.log(game(5));
